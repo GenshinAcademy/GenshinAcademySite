8b8f5b2ac82d8f7c5563f697da35b39721e80405
Jagerente <JustlPains@gmail.com>
2023-05-26 18:21:43 +0700

    add @unhead/vue;

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import App from '@/App.vue';
 import router from '@/router';
 import { i18n } from "@/localization";
 import VueGtag from "vue-gtag";
+import { createHead } from "@unhead/vue"
 
 const pinia = createPinia();
+const head = createHead()
 
 createApp(App)
   .use(i18n)
   .use(router)
   .use(pinia)
+  .use(head)
   .use(VueGtag, {
     config: { id: "G-0L5E763TLC" }
   })
